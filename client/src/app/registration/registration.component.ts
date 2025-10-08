c78236df11e3e4cf00aa894273d49f1117e8a258
username <wecp-dummy@gmail.com>
2025-10-08 12:02:21 +0000

    CARzY

--- client/src/app/registration/registration.component.ts
+++ client/src/app/registration/registration.component.ts
@@ -1,35 +1,51 @@
 
-
-
+ 
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { HttpService } from '../../services/http.service';
-// import { RegistrationSuccessDialogComponent } from '../registration-success-dialog/registration-success-dialog.component';
  
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
- 
- 
 export class RegistrationComponent {
   itemForm: FormGroup;
+  showSuccessPopup = false;
+ 
+  // Password validation flags
+  hasLowercase = false;
+  hasUppercase = false;
+  hasDigit = false;
+  hasSpecialChar = false;
+  hasMinLength = false;
  
   constructor(private fb: FormBuilder, private httpService: HttpService, private router: Router) {
-      this.itemForm = this.fb.group({
-        username: ['', [Validators.required, Validators.minLength(3)]],
-        email: ['', [Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
-        password: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};:\'",.<>/?]).{8,}$')]],
-        role: [null, Validators.required]
-      });
-    }
-    showSuccessPopup=false;
+    this.itemForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+        ]
+      ],
+      password: ['', [Validators.required]],
+      role: [null, Validators.required]
+    });
+  }
  
-//  Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
- 
+  // Real-time password validation
+  validatePassword(): void {
+    const password = this.itemForm.get('password')?.value || '';
+    this.hasLowercase = /[a-z]/.test(password);
+    this.hasUppercase = /[A-Z]/.test(password);
+    this.hasDigit = /\d/.test(password);
+    this.hasSpecialChar = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password);
+    this.hasMinLength = password.length >= 8;
+  }
  
   onSubmit(): void {
     if (this.itemForm.valid) {
@@ -38,7 +54,6 @@ export class RegistrationComponent {
           console.log('Registration successful', response);
           this.showSuccessPopup = true;
  
-          // Keep popup for 3 seconds, then redirect
           setTimeout(() => {
             this.showSuccessPopup = false;
             this.router.navigate(['/login']);
@@ -51,10 +66,11 @@ export class RegistrationComponent {
     }
   }
  
- 
   navigateToHome() {
     this.router.navigate(['/home']);
   }
- 
 }
  
+
+
+
